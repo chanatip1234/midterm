d8394616216ebddcf1bf3eadac872b607970917c
chanatip1234 <chanatip.auam@bumail.net>
2026-03-26 23:48:07 +0700

    BG_Sound

---
+++ Assets/Script/BackgroundMusic.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    private static BackgroundMusic instance;
+
+    void Awake()
+    {
+        
+        if (instance == null)
+        {
+            instance = this;
+            
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            
+            Destroy(gameObject);
+        }
+    }
+}
